b1795176cce36389d9202026200c3beef76fcdb8
edelbarno <barnoedel12@gmail.com>
2024-03-31 19:26:40 +0300

    add destructing lab

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+const [blackAndWhite, , black, pink, ] = farmAnimals.split(' ');
+console.log(moo, neigh, baa, oink, cluck);
+console.log(bessie, dolly, babe, little,);
+console.log(blackAndWhite, black, pink);
+
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, , v] = colors;
+const [ , , , , , indg,] = colors;
+console.log(red, orange, yellow, green, blue, indigo, violet);
+console.log(r, o, y, g, b, v);
+console.log(indg);
+
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +23,8 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+const {muppetName, color, song, job, partner} = muppet;
+
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +40,9 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+const { song2, song4 } = nestedMuppet.album.theMuppetMovie;
+const {nestedJob, nestedPartner} = nestedMuppet;
+
 
 // Strings
 
